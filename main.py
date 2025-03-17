24ed1a17b8e2f45ee9a2bf0e10ebf17a903ec6be
Ansel Rognlie <ansel@adadevelopersacademy.org>
2025-03-17 12:36:31 -0700

    include import

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+from random import randint
+
 CARDS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 SUITS = ["♥", "♦", "♠", "♣"]
 
 deck_of_cards = [f"{card}{suit}" for card in CARDS for suit in SUITS]
 
-# Implement a solution for shuffling a deck of cards without using random.shuffle()
-# Using randint() is ok
+# Implement a solution for shuffling a deck of cards
+# Use only randint() and no other imports
 def shuffle_deck(deck_of_cards):
     pass
